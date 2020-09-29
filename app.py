d39f36f5bef6e976674bd7ce5cd82eb71b27659f
Kanishk Mair <ksmair11@gmail.com>
2020-09-29 08:23:04 -0500

    Add case and death tabs

--- app.py
+++ app.py
@@ -19,33 +19,74 @@ df = pd.read_csv('covid_state_9_27.csv')
 
 df = df.groupby(['state']).max()
 df.reset_index(inplace=True)
+
+# Merging with the state code
+states_url = 'https://raw.githubusercontent.com/plotly/datasets/master/2014_usa_states.csv'
+pop_df = pd.read_csv(states_url)
+pop_df.drop(['Rank'], axis=1, inplace=True)
+df = df.merge(pop_df.iloc[:,:2], left_on='state', right_on='State') # Previous line can be avoided by selecting iloc[1:3]
+df.drop('State', axis=1, inplace=True)
+
 print(df)
-# geo_fips = 'https://raw.githubusercontent.com/PublicaMundi/MappingAPI/master/data/geojson/us-states.json'
-# with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
-#     counties = json.load(response)
 
-fig = px.choropleth(
-    df,
-    locations='state',
-    locationmode="USA-states",
-    scope='usa',
-    color='cases'
-    # ,hover_data=['state']
-)
+intro_markdown = '''
+&emsp;This app uses daily data posted by [The New York Times](https://github.com/nytimes/covid-19-data/blob/master/us-states.csv)
 
-app.layout = html.Div([
+&emsp;The tally of Cases and Deaths at present and forecasts based on time-series can be evaluated below
+'''
 
-   html.H1('Covid in the USA', className="mb-2", style={'text-align': 'center'}),
+def return_usa_map(df, col):
+    fig = px.choropleth(
+        df,
+        locations='Postal',
+        locationmode="USA-states",
+        scope='usa',
+        color=col,
+        hover_name='Postal'
+        # ,hover_data=['state']
+    )
+    fig.update_layout(
+        title_text = f'USA {col}'
+    )
+    return fig
 
+app.layout = html.Div(children=[
+
+    html.H1('Covid in the USA', className="mb-2", style={'text-align': 'center'}),
+    dcc.Markdown(children=intro_markdown),
  
-    dcc.Graph(
-        id='usa_map',
-        figure=fig
-        )
-    #    id='usa_map') 
+    dcc.Tabs(id='cases_or_deaths', value='cases', children=[
+            dcc.Tab(label='Cases', value='cases'),
+            dcc.Tab(label='Deaths', value='deaths')
+        ]),
+    html.Div(id='cases_or_deaths_content')
 
-])
-# )
+    ],
+    
+    # style={'background-image': 'url("/assets/corona-background.jpg")', 'background-color': 'rgba(255, 255, 255, 0.16)'}
+    style={'background-image': 'url("/assets/coronavirusbg.png")', 'background-size': '1400px 300px'}
+)
+@app.callback(Output('cases_or_deaths_content', 'children'),
+              [Input('cases_or_deaths', 'value')]
+)
+def render_tabs(tab):
+    if tab == 'cases':
+        return html.Div(children=[
+            dcc.Graph(
+            id='usa-map',
+            figure=return_usa_map(df, tab)
+            )
+        ])
+    elif tab == 'deaths':
+        return html.Div(children=[
+            dcc.Graph(
+            id='usa-map',
+            figure=return_usa_map(df, tab)
+            # figure=fig
+            )
+        ])
+
+
 # @app.callback(
 #     [Output(component_id='usa_map', component_property='figure')],
 #     [Input()]

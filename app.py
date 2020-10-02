50474b9ef7f0479b4227ea4de1dd4c8980175dd9
Kanishk Mair <ksmair11@gmail.com>
2020-10-02 08:26:48 -0500

    Add table

--- app.py
+++ app.py
@@ -3,11 +3,15 @@ import numpy as np
 import torch
 from urllib.request import urlopen
 import json
+import datetime
 
 import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 import dash 
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_table
 from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 # dash-table
@@ -27,7 +31,6 @@ pop_df.drop(['Rank'], axis=1, inplace=True)
 df = df.merge(pop_df.iloc[:,:2], left_on='state', right_on='State') # Previous line can be avoided by selecting iloc[1:3]
 df.drop('State', axis=1, inplace=True)
 
-print(df)
 
 intro_markdown = '''
 &emsp;This app uses daily data posted by [The New York Times](https://github.com/nytimes/covid-19-data/blob/master/us-states.csv)
@@ -35,7 +38,7 @@ intro_markdown = '''
 &emsp;The tally of Cases and Deaths at present and forecasts based on time-series can be evaluated below
 '''
 
-def return_usa_map(df, column):
+def return_usa_plots(df, column):
 
     # Latest total
     state_df = df.groupby(['state']).max()
@@ -57,12 +60,81 @@ def return_usa_map(df, column):
 
     # Plot total cases/deaths by date
     date_df = df.groupby('date').sum()
-    print(date_df)
-    fig_usa_total = px.scatter(
+
+    fig_usa_total = make_subplots(specs=[[{"secondary_y": True}]])
+    
+    fig_usa_total.add_trace(
+        go.Scatter(
         x = date_df.index,
-        y = date_df[column]
+        y = date_df[column].diff(),
+        name=f"Daily {column}",
+        fill='tozeroy'
+        ), secondary_y = True
     )
+    
+    fig_usa_total.add_trace(
+        go.Scatter(
+        x = date_df.index,
+        y = date_df[column],
+        name=f"Total {column}",
+        
+        # labels=dict(x="", y=f"Total {column}")
+        ), secondary_y = False
+    )
+
+    fig_usa_total.update_layout(xaxis_range=[datetime.datetime(2020, 1, 10),
+                               datetime.date.today() + datetime.timedelta(days=10)])
+
     return fig_usa_map, fig_usa_total
+
+def return_usa_stats(df, column):
+    new_df = df.loc[:, ['date', 'state', column]].set_index('date')
+    state_df = new_df.groupby('state')
+
+    # Total and % of total affected
+    latest_df = state_df.agg(['last'])
+    latest_df = latest_df.droplevel(1, axis=1)  # removing agg of last from column
+    latest_df = pop_df.merge(latest_df, left_on='State', right_index=True).drop(['Postal'], axis=1)
+    # print(latest_df)
+    latest_df['Total per capita (%)'] = latest_df[column]/latest_df['Population']*100
+    latest_df.rename({column: f'Total {column}'}, axis = 1, inplace=True)
+    
+    # Growth over the past week
+    # rolling_sum = state_df.rolling(5).sum()  # Past week count
+    # rolling_sum = rolling_sum.reset_index().groupby('state').agg('last')
+    # rolling_sum = rolling_sum.drop('date', axis=1).rename({column: f'Increase in a week'}, axis=1)
+    weekly_increase = state_df.apply(lambda x: (x[column].iloc[-1]-x[column].iloc[-8]))
+    weekly_increase = pd.DataFrame(data=weekly_increase, columns=['Increase in a week'])
+    df = latest_df.round(decimals=3).merge(weekly_increase, left_on='State', right_index=True).drop(['Population'], axis=1)
+
+    # Plotly
+    stats_table = dash_table.DataTable(
+        id='table',
+        columns=[{"name": i, "id": i} for i in df.columns],
+        data=df.to_dict('records'),
+        filter_action="native",
+        sort_action="native",
+        row_selectable="multi", 
+        column_selectable="single",
+        selected_columns=[],
+        selected_rows=[],
+        page_action="native",
+        page_current=0, 
+        page_size=6,
+        style_cell={'minWidth': 100, 'maxWidth': 100, 'width': 100},
+        style_cell_conditional=[
+            {
+                'if': {'column_id': c},
+                'textAlign': 'left'
+            } for c in {'State'}
+        ],
+        style_data={
+            'whitespace': 'normal',
+            'height': 'auto'
+        }
+    )
+
+    return stats_table
 
 app.layout = html.Div(children=[
     html.Div(children=[        
@@ -90,14 +162,18 @@ app.layout = html.Div(children=[
               [Input('cases_or_deaths', 'value')]
 )
 def render_tabs(tab):
-    usa_map, usa_total = return_usa_map(df, tab)
+    usa_map, usa_total = return_usa_plots(df, tab)
 
-    usa_plots = html.Div(
+    stats_table = return_usa_stats(df, tab)
+    # print(table)
+
+    usa_plots = html.Div([
+        html.Div(
         children=[
             html.Div(children=[
                     dcc.Graph(
                     id='usa-map',
-                    figure=usa_map   # return_usa_map(df, tab)
+                    figure=usa_map   
                     )
                 ], className='col-md-6 px-0'
             ),
@@ -108,11 +184,25 @@ def render_tabs(tab):
                     )
                 ], className='col-md-6 px-0'
             )
+        ], className='row mx-0'),
+
+        html.Div(
+        children=[
+            html.Div(children=[stats_table], className='col-md-6'
+            ),
+            html.Div(children=[
+                    dcc.Graph(
+                    id='usa-map',
+                    figure=usa_total 
+                    )
+                ], className='col-md-6 px-0'
+            )
         ], className='row mx-0')
 
+    ]
+    )
     return usa_plots
     
 
 if __name__ == "__main__":
     app.run_server(debug=True)
-    # pass
